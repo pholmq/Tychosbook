2581eb3d387b83518b77e9871b2e223055ad47e5
pholmq <pholmq@gmail.com>
2021-10-20 20:13:59 +0200

    built with Nextra

--- theme.config.js
+++ theme.config.js
@@ -62,6 +62,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: "Edit this page on GitHub",
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>MIT {new Date().getFullYear()} Built with © Nextra.</>,
   unstable_faviconGlyph: "🐌"
 };
